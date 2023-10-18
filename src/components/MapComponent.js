a83117cf2185d858423d883f21f73a0283a98ef5
madhatter1999 <anthonyhealy90@gmail.com>
2023-10-18 08:02:51 -0300

    removed the box

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,6 +1,6 @@
 // /Components/MapComponent.js Anthony Healy
 import React, { useRef, forwardRef, useImperativeHandle } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
 
 const MapComponent = forwardRef(({ children, center, zoom }, ref) => {
   const mapInstance = useRef(null);
@@ -9,6 +9,14 @@ const MapComponent = forwardRef(({ children, center, zoom }, ref) => {
     getLeafletElement: () => mapInstance.current,
   }));
 
+  // Extract positions from children (assuming they are markers)
+  const positions = React.Children.map(children, child => {
+    if (child.type === "Marker") {
+      return child.props.position;
+    }
+    return null;
+  }).filter(pos => pos !== null);
+
   return (
     <MapContainer ref={mapInstance} center={center} zoom={zoom} style={{ width: '100%', height: '100%' }}>
       <TileLayer
@@ -16,6 +24,8 @@ const MapComponent = forwardRef(({ children, center, zoom }, ref) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {children}
+      {/* Render polyline using the extracted positions */}
+      <Polyline positions={positions} color="lime" />
     </MapContainer>
   );
 });
